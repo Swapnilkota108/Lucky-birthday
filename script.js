29339189c01f57de1544c3e247de95d21806318b
Swapnil kota <83860595+Swapnilkota108@users.noreply.github.com>
2021-08-14 11:35:31 +0530

    update

---
+++ script.js
@@ -0,0 +1,44 @@
+const button = document.querySelector('.btn');
+
+const dateInput = document.querySelector('.date');
+const luckyNumberInput = document.querySelector('.luckyNum');
+const output = document.querySelector('.output');
+const cake = document.querySelector('.cake');
+
+function calculateSum(dob){
+   dob = dob.replaceAll('-','');
+  let sum=0;
+  for(let i=0;i<dob.length;i++){
+    sum = sum + Number(dob.charAt(i));
+  }
+  return sum;
+}
+
+function compareValues(sum,value){
+    
+  if(sum%value===0){
+     output.innerText='Congrats,your birth date is a lucky number 🥳 🤩';
+     cake.style.display = 'block';
+  }
+  else{
+    output.innerText= 'You create your own luck 🤗 😉';
+  }
+}
+
+
+
+
+
+
+
+
+
+
+button.addEventListener('click',function clickhandler(){
+  const dob = dateInput.value;
+  const sum = calculateSum(dob);
+  const value=luckyNumberInput.value;
+  cake.style.display = 'none';
+  compareValues(sum,value);
+}
+)
